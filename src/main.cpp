25dad44537096c3a1120525e20db67d68b5054a0
Peeju <paul_peeju@yahoo.com>
2023-12-28 17:28:01 +0200

    ADC transmit resolved

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,11 @@
 
 #define TX_GPIO_NUM   GPIO_NUM_14
 #define RX_GPIO_NUM   GPIO_NUM_27
-#define LOG_LEVEL LOG_LEVEL_VERBOSE
+#define LOG_LEVEL LOG_LEVEL_NOTICE
 
 //todo: convert MPU values from double to int
-//!debug ADC conversion to can
+//!debug tx_msg_transmit -- probably needs extended frame;
+//todo: can transmit error more conclusive
 
 
 
@@ -41,7 +42,7 @@ void setup() {
   status = mpu.begin();
 
    if(status!=0) {
-    Log.error("MPU6050 not initialized - ERROR STATUS: %d", status);
+    Log.errorln("MPU6050 not initialized - ERROR STATUS: %d", status);
    }
    else {
     Log.notice("MPU status: %d\n", status);
@@ -70,45 +71,55 @@ void setup() {
 
 void loop() {
   twai_message_t tx_msg_mpu;
-  tx_msg_mpu.data_length_code=6;
+  tx_msg_mpu.data_length_code=11;
   tx_msg_mpu.identifier=0x111;
   tx_msg_mpu.flags=CAN_MSG_FLAG_NONE;
 
   twai_message_t tx_msg_damp;
-  tx_msg_damp.data_length_code=4;
+  tx_msg_damp.data_length_code=8;
   tx_msg_damp.identifier=0x112;
   tx_msg_damp.flags=CAN_MSG_FLAG_NONE;
   
   mpu.update();
 
-  #ifdef LOG_LEVEL_VERBOSE
-  int aX=mpu.getAngleX();
-  int aY=mpu.getAngleY();
-  int aZ=mpu.getAngleZ();
-  int acX=mpu.getAccX();
-  int acY=mpu.getAccY();
-  int acZ=mpu.getAccZ();
+  #if LOG_LEVEL==LOG_LEVEL_VERBOSE
+  double aX=mpu.getAngleX();
+  double aY=mpu.getAngleY();
+  double aZ=mpu.getAngleZ();
+  double acX=mpu.getAccX();
+  double acY=mpu.getAccY();
+  double acZ=mpu.getAccZ();
   Log.verboseln("Ax:%F, Ay:%F, Az:%F, ACCx:%F, ACCy:%F, ACCz:%F", aX, aY, aZ, acX, acY, acZ);
   
   int d1 = damper1.getVoltage();
-  // int d2 = damper2.getVoltage();
-  // int d3 = damper3.getVoltage();
-  // int d4 = damper4.getVoltage();
-  Log.verboseln("\n D1: %d, D2: %d, D3: %d, D4: %d \n\n", d1, d1, d1, d1);
+  int d2 = damper2.getVoltage();
+  int d3 = damper3.getVoltage();
+  int d4 = damper4.getVoltage();
+  Log.verboseln("D1: %d, D2: %d, D3: %d, D4: %d", d1, d1, d1, d1);
   //Log.verboseln("\n D1: %d\n\n", d1);
   
  
-  tx_msg_damp.data[0]=d1;
-  tx_msg_damp.data[1]=d1;
-  tx_msg_damp.data[2]=d1;
-  tx_msg_damp.data[3]=d1;
+  tx_msg_damp.data[0]=d1/100;
+  tx_msg_damp.data[1]=d1%100;
+  tx_msg_damp.data[2]=d2/100;
+  tx_msg_damp.data[3]=d2%100;
+  tx_msg_damp.data[4]=d3/100;
+  tx_msg_damp.data[5]=d3%100;
+  tx_msg_damp.data[6]=d4/100;
+  tx_msg_damp.data[7]=d4%100;
 
-  tx_msg_mpu.data[0]=aX;
-  tx_msg_mpu.data[1]=aY;
-  tx_msg_mpu.data[2]=aZ;
-  tx_msg_mpu.data[3]=acX;
-  tx_msg_mpu.data[4]=acY;
-  tx_msg_mpu.data[5]=acZ;
+  tx_msg_mpu.data[0]=int(aX*100)/100;
+  tx_msg_mpu.data[1]=int(aX*100)%100;
+  tx_msg_mpu.data[2]=int(aY*100)/100;
+  tx_msg_mpu.data[3]=int(aY*100)%100;
+  tx_msg_mpu.data[3]=int(aZ*100)/100;
+  tx_msg_mpu.data[4]=int(aZ*100)%100;
+  tx_msg_mpu.data[5]=int(acX*100)/100;
+  tx_msg_mpu.data[6]=int(acX*100)%100;
+  tx_msg_mpu.data[7]=int(acY*100)/100;
+  tx_msg_mpu.data[8]=int(acY*100)%100;
+  tx_msg_mpu.data[9]=int(acZ*100)/100;
+  tx_msg_mpu.data[10]=int(acZ*100)%100;
   #else
   tx_msg_damp.data[0]=damper1.getVoltage();
   tx_msg_damp.data[1]=damper2.getVoltage();
@@ -137,11 +148,14 @@ void loop() {
   }
 
   
-  status = can_transmit(&tx_msg_mpu, portMAX_DELAY);
+  status = can_transmit(&tx_msg_mpu, pdMS_TO_TICKS(1000));
+  Serial.println(status);
+  Serial.println(esp_err_to_name(status));
    if(status==ESP_OK) {
     Log.noticeln("Can message sent");
   }
   else {
+    Serial.println(esp_err_to_name(status));
     Log.errorln("Can message sending failed with error code: %s ;\nRestarting CAN driver", esp_err_to_name(status));
     can_stop();
     can_driver_uninstall();
